34391cc079f872fb0f47d012d543b0d88727d308
Евгений Шевелев <sheveleveester@gmail.com>
2022-03-30 12:49:39 +0300

    fix styling problems

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,8 @@
 import prompt
+"""Запрашивает имя игрока."""
 
 
 def welcome_user():
     """Запрашивает имя игрока."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-    
+    print('Hello, {0}'.format(name))
--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+"""Запуск основных функций."""
 from brain_games.cli import welcome_user
 
 
 def main():
     """Вызов основных функций программы."""
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
     welcome_user()
 
 
