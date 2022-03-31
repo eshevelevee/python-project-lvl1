c37ea36b67c9a599a27ba76d99c5e5274b4eabbc
Евгений Шевелев <sheveleveester@gmail.com>
2022-03-31 18:39:09 +0300

    fixing pep style brain-games-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python
+"""Игра в определение четности числа."""
 from random import randint
 from brain_games.cli import welcome_user
 
 import prompt
 
 
-def even_question(a):
-    """Игра на определение четности числа"""
+def even_question(number_of_tries):
+    """Игра на определение четности числа
+
+    Аргументы:
+    :param number_of_tries: количество успешных попыток подряд для выигрыша в игре
+    :type number_of_tries: int
+    :return: 0 если игрок ошибся
+    """
     print('Welcome to the Brain Games!')
     name = welcome_user()
     total_correct = 0
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    while total_correct != a:
+    while total_correct != number_of_tries:
         number = randint(0, 100)
-        print("Question: {0}".format(number))
+        print('Question: {0}'.format(number))
         answer = prompt.string('Your answer: ')
-        if number % 2 == 0 and answer == "yes":
+        if number % 2 == 0 and answer == 'yes':
             print('Correct!')
             total_correct += 1
-        elif number % 2 != 0 and answer == "no":
+        elif number % 2 != 0 and answer == 'no':
             print('Correct!')
             total_correct += 1
         else:
-            print(''''yes' is wrong answer ;(. Correct answer was 'no'. \n
-Let's try again, {0}!'''.format(name))
+            message = '''
+'yes' is wrong answer ;(. Correct answer was 'no'. \n
+Let's try again, {0}!
+'''.format(name)
+            print(message)
             return 0
     print('Congratulations, {0}!'.format(name))
 
